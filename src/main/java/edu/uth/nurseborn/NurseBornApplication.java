35133efa5e1f212479eca05a4645a388b5571dec
Binh-Nguyen <binhb3573@gmail.com>
2025-03-09 21:53:22 +0700

    Khởi tạo dự án NurseBorn

---
+++ src/main/java/edu/uth/nurseborn/NurseBornApplication.java
@@ -0,0 +1,13 @@
+package edu.uth.nurseborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseBornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurseBornApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uth/nurseborn/NurseBornApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.nurseborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseBornApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
